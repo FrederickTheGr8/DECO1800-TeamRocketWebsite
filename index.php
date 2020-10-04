80748bc4d475178e6c8e6843eb751693fd5d6768
Luke Hewitt <sensator@gmail.com>
2020-10-04 14:58:43 +1000

    basic layout

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src ="js/bootstrap.min.js"></script>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<title>What's that QLD Animal</title>
+</head>
+
+<body>
+	<header class="landingpage">
+		<div class="container h-100">
+			<div class="row h-100 align-items-center justify-content-center text-center">
+				<div class="col-lg-10 align-self-end">
+					<h1 class="text-uppercase text-white font-weight-bold">What's that QLD Animal</h1>
+					<hr class="divider my-4" />
+				</div>
+				<div class="col-lg-6 align-self-baseline">
+					<p class="text-white bg-success font-weight-light mb-5">How many animals can you guess in these pictures?</p>
+					<a class="btn btn-primary btn-xl js-scroll-trigger" href="game.php">Start Game<a>
+				</div>
+			</div>
+		</div>
+	</header>
+</body>
+
+
